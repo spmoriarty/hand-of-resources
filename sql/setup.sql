43c8c0884df1933f466f6761cabacf5ba0ae628e
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-05 09:40:47 -0700

    corvid sql built for SQL suite

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,21 @@
 -- Use this file to define your SQL tables
+
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXIST corvid
+
+
+
+
+
+CREATE TABLE corvid (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    color VARCHAR NOT NULL
+);
+
+INSERT INTO corvid (name, color) VALUES
+('Raven', 'Black'),
+('Crow', 'Black'),
+('Jay', 'Blue'),
+('Cardinal', 'Red'),
+('Magpie', 'Black and White');
